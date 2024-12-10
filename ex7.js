05b2e7b94f8b933cd8fee69f2fc57207501d6bf5
Ricardo Pereira <jrm.pereira@campus.fct.unl.pt>
2024-12-10 11:50:16 +0000

    exercise 7 done

--- ex7.js
+++ ex7.js
@@ -1,20 +1,55 @@
 var string = 'asdfgfdsa';
 
+var text = string.toLowerCase();
+
+function isPalindromeIterative(word) {
+
+    size = word.length;
+
+    let end = size - 1;
+
+    for (let i = 0; i < size / 2; i++, end--) {
+
+        if (word.charAt(i) !== word.charAt(end)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPalindromeIterative(text));
 
 
+function isPalindromeRec(word) {
 
 
+    size = word.length;
 
+    if (word === text) {
+        startingIndex = 0
+        endIndex = size - 1;
 
+    } else if (size <= 1)
+        return true;
 
+    leftChar = word.charAt(startingIndex);
+    rightChar = word.charAt(endIndex);
 
+    if (size == 2)
+        return leftChar === rightChar;
 
+    else {
 
+        newWord = word.substring(startingIndex + 1, endIndex);
 
+        size = newWord.length;
+        endIndex = size - 1;
 
+        console.log("new word: " + newWord);
+        console.log("start index: " + newWord.charAt(startingIndex));
+        console.log("end index: " + newWord.charAt(endIndex));
+        return isPalindromeRec(newWord);
+    }
 
-
-
-
-
-console.log('');
+}
+console.log(isPalindromeRec(text));
