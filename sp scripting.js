392342f50b250110bd62ce6e646ac5bd4855fc19
Ricardo Pereira <jrm.pereira@campus.fct.unl.pt>
2025-02-01 19:49:24 +0000

    widget done

--- sp scripting.js
+++ sp scripting.js
@@ -2,26 +2,28 @@
 
 	data.vehicles = [];
 
+	if(!options.type)
+		options.type = "CaR";
+
 
 	var vehiclesGR = new GlideRecord('x_512735_bmw_gen_cmdb_ci_vehicle');
 	vehiclesGR.addEncodedQuery("u_type=" + options.type + "^ORDERBYDESCu_production_start");
+	//vehiclesGR.addEncodedQuery("u_type=motorcycle^ORDERBYDESCu_production_start");
+	//vehiclesGR.addEncodedQuery("u_type=car^ORDERBYDESCu_production_start");
 	vehiclesGR.setLimit(options.maximum_records_to_display);
 	vehiclesGR.query();
 	while (vehiclesGR.next()) {
 		var vehiclesObj = {};
 
-		$sp.getRecordDisplayValues(vehiclesObj, vehiclesGR, 'name,u_type,sys_id');
+		$sp.getRecordDisplayValues(vehiclesObj, vehiclesGR, 'name,sys_id');
 
-		vehiclesObj.vehicle_name = vehiclesGR.getValue('name');
 		vehiclesObj.vehicle_type = vehiclesGR.getValue('u_type');
+		vehiclesObj.startProd = vehiclesGR.getValue('u_production_start');
 
 		console.log(vehiclesObj.vehicle_type + " " + typeof vehiclesObj.vehicle_type);
 
-			data.vehicles.push(car)
-		
+		data.vehicles.push(vehiclesObj);
+
 	}
-
-
-	///com base no options.type definir a logica
 }
 )();
